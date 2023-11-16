48463dc9ef9094b032a8dacf99033d78641dbce5
Anthony Vinay Kumar S <anthonyvinaykumar1992@gmail.com>
2023-11-17 01:53:16 +0200

    first commit

---
+++ src/main/java/com/savk/workout/reportinggatewaysimulation/ReportingGatewaySimulationApplication.java
@@ -0,0 +1,13 @@
+package com.savk.workout.reportinggatewaysimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingGatewaySimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingGatewaySimulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/savk/workout/reportinggatewaysimulation/ReportingGatewaySimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savk.workout.reportinggatewaysimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingGatewaySimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
